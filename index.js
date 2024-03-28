b40956c08b34e7f75f896e56f0c2e5b56c72e8e4
antmic <antoni.michera@gmail.com>
2024-03-28 13:38:06 +0100

    forgot again about localhost

--- index.js
+++ index.js
@@ -34,8 +34,7 @@ const buttons = [
 	...keyboard,
 ];
 
-//const serverUrl = 'https://diffle-be-lingering-log-9938.fly.dev';
-const serverUrl = 'http://localhost:3000';
+const serverUrl = 'https://diffle-be-lingering-log-9938.fly.dev';
 const renderTimeout = 300;
 const errorTimeout = 2000;
 
