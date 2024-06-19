712948a7e362d1223524731522682059bd9db992
antmic <antoni.michera@gmail.com>
2024-06-20 00:47:30 +0200

    improved tests

--- test.js
+++ test.js
@@ -1,21 +1,23 @@
 import { validator } from './validator.js';
 
 const testCases = [
-	// { input: 'fantastyka', word: 'migotać' },
-	{ input: 'trelee', word: 'bambetle' },
-	// { input: 'ale', word: 'bambetle' },
-	// { input: 'pajac', word: 'trajan' },
-	// { input: 'klaskała', word: 'ekierka' },
-	{ input: 'pingwin', word: 'pininfarina' },
-	{ input: 'ciocikawskaci', word: 'cimcietka' },
-	{ input: 'ciekawska', word: 'metka' },
-	{ input: 'telegram', word: 'lemur' },
-	{ input: 'zdrowa', word: 'zaprowadzać' },
-	{ input: 'tygrysy', word: 'dyplomatyczny' },
-	{ input: 'wydatnie', word: 'uwydatnienie' },
-	{ input: 'gnie', word: 'uwydatnienie' },
-	{ input: 'sklonowanie', word: 'skomplikowanie' },
+	{ input: 'fantastyka', word: 'migotać', shouldBe: 'fan<(T)(A)>styka' },
+	{ input: 'trelee', word: 'bambetle', shouldBe: '(T)re<(L)E>(E)*' },
+	{ input: 'ale', word: 'bambetle', shouldBe: '(A)<(L)(E)>*' },
+	{ input: 'pajac', word: 'trajan', shouldBe: 'p<(A)-(J)-(A)>c' },
+	{ input: 'klaskała', word: 'ekierka', shouldBe: '(K)las<(K)a>ł(A)*' },
+	{ input: 'pingwin', word: 'pininfarina', shouldBe: '*<(P)-(I)-(N)>gw<(I)(N)>' },
+	{ input: 'ciekawska', word: 'metka', shouldBe: 'ci(E)kaws<(K)(A)>*' },
+	{ input: 'telegram', word: 'lemur', shouldBe: 'te<(L)(E)>g(R)aM' },
+	{ input: 'zdrowa', word: 'zaprowadzać', shouldBe: '*(Z)(D)<R-O--W-A>' },
+	{ input: 'tygrysy', word: 'dyplomatyczny', shouldBe: '<(T)(Y)>grYs(Y)*' },
+	{ input: 'wydatnie', word: 'uwydatnienie', shouldBe: '<(W)-(Y)--(D)--(A)--(T)--(N)--(I)-(E)>' },
+	{ input: 'gnie', word: 'uwydatnienie', shouldBe: 'g<(N)-(I)-(E)>*' },
+	{ input: 'sklonowanie', word: 'skomplikowanie', shouldBe: '*<(S)(K)>(L)On<(O)-(W)--(A)--(N)--(I)-(E)>*' },
+	{ input: 'emanacja', word: 'abstynencja', shouldBe: '(E)mA(N)a<(C)-(J)-(A)>*' },
 ];
+
+let failCount = 0;
 
 testCases.forEach(testCase => {
 	let result = validator(testCase.input, testCase.word);
@@ -59,8 +61,23 @@ testCases.forEach(testCase => {
 			show.push('*');
 		}
 	});
-	console.log(testCase.word);
-	console.log(testCase.input);
-	console.log(show.join(''));
-	console.log(result);
+	console.log('word: ', testCase.word);
+	console.log('input: ', testCase.input);
+	console.log('result: ', show.join(''));
+	console.log('should: ', testCase.shouldBe);
+	if (show.join('') === testCase.shouldBe) {
+		console.log('OK');
+	} else {
+		failCount++;
+		console.error('FAIL');
+	}
+	console.log('----------------------------------');
+
+	//console.log(result);
 });
+
+if (failCount === 0) {
+	console.log('All tests passed.');
+} else {
+	console.error(`${failCount}/${testCases.length} tests failed.`);
+}
